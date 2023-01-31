6f4308ca1e84d28b353546f40c41d403a8ccf629
R H <96940361+RHC0DE@users.noreply.github.com>
2023-01-31 16:41:27 +0100

    Initial Commit

---
+++ Ace Guest/Ace_GuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ace_GuestApp.swift
+//  Ace Guest
+//
+//  Created by Guest User on 31/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Ace_GuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
